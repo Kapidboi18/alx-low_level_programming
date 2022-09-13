7dc431b8daa8063847aca0310b892bc4aab3a9f3
“kapidboi18” <clintonchinaza17@gmail.com>
2022-09-13 12:42:23 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,23 @@
-#!/bin/bash
+#include "main.h"
+/**
+ *
+ * main -prints putchar
+ *
+ * Discription: prints _putchar
+ * Return: (0) (success)
+ */
+int main(void)
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchat('r');
+_putchar('\n');
+Return ('0');
+}
+!/bin/bash
 su putchar
 
