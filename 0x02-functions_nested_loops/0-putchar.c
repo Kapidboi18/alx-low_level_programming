616954c2264b7876d79a8cd214ccbdd932703bd4
Clinton Siloko <111067926+Kapidboi18@users.noreply.github.com>
2022-09-13 20:01:04 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,20 @@
-#!/bin/bash
-su putchar
+#include <stdio.h>
+/**
+ * main - Entry point
+ * code description
+ * Return: Always 0 (Success)
+ */
+int main(void)
 
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
