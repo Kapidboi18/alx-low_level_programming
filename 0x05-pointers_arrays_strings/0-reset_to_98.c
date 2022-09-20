4bd337d5d9ac434445c973347849924d619922ea
“kapidboi18” <clintonchinaza17@gmail.com>
2022-09-20 14:48:08 -0700

    0-reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,3 +1,12 @@
-#!/bin/bash
-su 0-reset_to_98.c
+#include "main.h"
 
+/**
+ * reset_to_98 - function that updates value of a parameter it points to to 98
+ * @n: pointer reset to 98
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
