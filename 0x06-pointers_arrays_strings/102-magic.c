632bf32d25e0fa94f0330c471b985614d70b9f70
“kapidboi18” <clintonchinaza17@gmail.com>
2022-09-22 16:27:11 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,7 +11,6 @@ int main(void)
 	/**
 	 * write your line of code here...
 	 * Remember:
-	 * main - magic
 	 * - you are not allowed to use a
 	 *   - you are not allowed to modify p
 	 *   - only one statement
